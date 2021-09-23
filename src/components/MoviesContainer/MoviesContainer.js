649feee1623d177a420047a209f177adbc1eef3c
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-09-23 12:15:15 -0400

    Create individual movie cards for each movie

--- src/components/MoviesContainer/MoviesContainer.js
+++ src/components/MoviesContainer/MoviesContainer.js
@@ -2,10 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import MovieCard from "../MovieCard/MovieCard";
 
-const MoviesContainer = props => {
-  return <main className="movieContainer"></main>;
+const MoviesContainer = (props) => {
+  const { movieData } = props;
+
+  return (
+    <main className="movieContainer">
+      {movieData.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />
+      ))}
+    </main>
+  );
 };
 
-MoviesContainer.propTypes = {};
+MoviesContainer.propTypes = {
+  movieData: PropTypes.array.isRequired
+};
 
 export default MoviesContainer;
